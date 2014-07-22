a8f5d22c33547c756cdeae43be60471420da13c8
Emil Lundberg <lundberg.emil@gmail.com>
2014-07-22 13:05:53 +0200

    Add hello.cpp for lab1 task 1.1

---
+++ 1lab/intro/hello.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    string addressee = "world";
+    int helloTimes = 1;
+
+    if(argc > 1) {
+        int helloArgIndex;
+        int i = atoi(argv[1]);
+        if(i > 0) {
+            helloTimes = i;
+            helloArgIndex = 2;
+        } else {
+            helloArgIndex = 1;
+        }
+
+        if(argc > helloArgIndex) {
+            addressee = argv[helloArgIndex];
+        }
+    }
+
+    cout << "Hello";
+    for(int i=0; i<helloTimes; ++i) {
+        cout << " " << addressee;
+    }
+    cout << "!" << endl;
+}
