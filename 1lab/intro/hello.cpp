3ac94636a4eb408314fd9ff4295cc09e22c5fa02
Emil Lundberg <lundberg.emil@gmail.com>
2014-07-23 15:02:21 +0200

    Whitespace: Replace 4 spaces with 2 spaces

--- 1lab/intro/hello.cpp
+++ 1lab/intro/hello.cpp
@@ -5,27 +5,27 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    string addressee = "world";
-    int helloTimes = 1;
+  string addressee = "world";
+  int helloTimes = 1;
 
-    if(argc > 1) {
-        int helloArgIndex;
-        int i = atoi(argv[1]);
-        if(i > 0) {
-            helloTimes = i;
-            helloArgIndex = 2;
-        } else {
-            helloArgIndex = 1;
-        }
-
-        if(argc > helloArgIndex) {
-            addressee = argv[helloArgIndex];
-        }
+  if(argc > 1) {
+    int helloArgIndex;
+    int i = atoi(argv[1]);
+    if(i > 0) {
+      helloTimes = i;
+      helloArgIndex = 2;
+    } else {
+      helloArgIndex = 1;
     }
 
-    cout << "Hello";
-    for(int i=0; i<helloTimes; ++i) {
-        cout << " " << addressee;
+    if(argc > helloArgIndex) {
+      addressee = argv[helloArgIndex];
     }
-    cout << "!" << endl;
+  }
+
+  cout << "Hello";
+  for(int i=0; i<helloTimes; ++i) {
+    cout << " " << addressee;
+  }
+  cout << "!" << endl;
 }
