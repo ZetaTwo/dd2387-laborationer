1ead9b825ad3be17bb2ca5e95aeef35d076aec93
Emil Lundberg <lundberg.emil@gmail.com>
2014-07-27 12:57:56 +0200

    Implement Hypercube in declaration

--- 1lab/vector/hypercube.hpp
+++ 1lab/vector/hypercube.hpp
@@ -3,23 +3,13 @@
 template<class T, size_t n>
 class Hypercube : public Vector<Hypercube<T, n-1> > {
 public:
-  Hypercube();
-  Hypercube(size_t s);
+  Hypercube() : Vector<Hypercube<T, n-1> >() {};
+  Hypercube(size_t s) : Vector<Hypercube<T, n-1> >(s, Hypercube<T, n-1>(s)) {};
 };
 
 template<class T>
 class Hypercube<T, 1> : public Vector<T> {
 public:
-  Hypercube();
-  Hypercube(size_t s);
+  Hypercube() : Vector<T>() {};
+  Hypercube(size_t s) : Vector<T>(s) {};
 };
-
-template<class T, size_t n>
-Hypercube<T, n>::Hypercube() : Vector<Hypercube<T, n-1> >() {};
-template<class T, size_t n>
-Hypercube<T, n>::Hypercube(size_t s) : Vector<Hypercube<T, n-1> >(s, Hypercube<T, n-1>(s)) {};
-
-template<class T>
-Hypercube<T, 1>::Hypercube() : Vector<T>() {};
-template<class T>
-Hypercube<T, 1>::Hypercube(size_t s) : Vector<T>(s) {};
