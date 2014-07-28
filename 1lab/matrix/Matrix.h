e67f94fdb04b8a08fb8a382e791b52a26a2d579f
Calle Svensson <calle.svensson@zeta-two.com>
2014-07-28 23:27:15 +0200

    Added matrix projects
    Added matrix tests

---
+++ 1lab/matrix/Matrix.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+#include "../vector/kth_cprog_vektor.cpp"
+
+class Matrix
+{
+ public:
+    typedef unsigned int index;
+
+    class matrix_row : private Vector< int >
+    {
+    public:
+        matrix_row(std::size_t s = 0) : Vector< int >( s ) {}
+        using Vector<int>::operator [];
+    private:
+        friend std::istream& operator>>( std::istream&, Matrix& );
+    };
+
+    Matrix();
+    Matrix(const Matrix& other);
+    explicit Matrix(std::size_t size);
+    Matrix(std::size_t rows, std::size_t cols);
+    Matrix(std::string serialized);
+    ~Matrix();
+
+    Matrix& operator= ( const Matrix& );
+    bool operator== (const Matrix&) const;
+    bool operator!= (const Matrix&) const;
+    Matrix operator+ ( const Matrix& ) const;
+    Matrix operator* ( const Matrix& ) const;
+    Matrix operator* ( int ) const;
+    Matrix operator-( const Matrix& ) const;
+    Matrix operator-( ) const;
+
+
+    Matrix transpose( ) const;
+
+    matrix_row& operator[]( index i );
+    const matrix_row& operator[]( index i ) const;
+
+    std::size_t rows() const;
+    std::size_t cols() const;
+
+ protected:
+ private:
+    Vector< matrix_row >        m_vectors;
+    std::size_t                 m_rows;
+    std::size_t                 m_cols;
+
+    void add_row( );            // Non mandatory help function
+    friend std::istream& operator>> ( std::istream&, Matrix& );
+};
+
+std::istream& operator>> ( std::istream&, Matrix& );
+std::ostream& operator<< ( std::ostream&, Matrix& );
+Matrix operator* ( int, const Matrix& );
