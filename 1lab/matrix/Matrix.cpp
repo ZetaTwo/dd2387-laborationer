36cb5f899829eca08b4a8e752a6ccf2151185ae4
Calle Svensson <calle.svensson@zeta-two.com>
2014-07-29 19:02:21 +0200

    Implemented some functions in Matrix

--- 1lab/matrix/Matrix.cpp
+++ 1lab/matrix/Matrix.cpp
@@ -1,24 +1,27 @@
 #include "Matrix.h"
 
 
-Matrix::Matrix()
+Matrix::Matrix() : m_cols(0), m_rows(0)
+{
+}
+
+Matrix::Matrix(const Matrix& other) : m_cols(other.m_cols), m_rows(other.m_rows), m_vectors(m_rows)
 {
 
 }
 
-Matrix::Matrix(const Matrix& other)
+Matrix::Matrix(std::size_t size) : m_cols(size), m_rows(size), m_vectors(m_rows, matrix_row(m_cols))
 {
-
+  if (size == 0) {
+    throw std::invalid_argument("Matrix size must be at least 1");
+  }
 }
 
-Matrix::Matrix(std::size_t size)
+Matrix::Matrix(std::size_t rows, std::size_t cols) : m_cols(cols), m_rows(rows), m_vectors(m_rows, matrix_row(m_cols))
 {
-
-}
-
-Matrix::Matrix(std::size_t rows, std::size_t cols)
-{
-
+  if (rows == 0 || cols == 0) {
+    throw std::invalid_argument("Matrix must have at least one row and column");
+  }
 }
 
 Matrix::Matrix(std::string serialized)
@@ -31,35 +34,48 @@ Matrix::~Matrix()
 
 }
 
-Matrix& Matrix::operator=(const Matrix&)
+Matrix& Matrix::operator=(const Matrix& other)
 {
-  Matrix a;
-  return a;
+  if (this == &other) {
+    return *this;
+  }
+
+  m_vectors = other.m_vectors;
+  m_cols = other.m_cols;
+  m_rows = other.m_rows;
+  
+  return *this;
 }
 
-bool Matrix::operator==(const Matrix&) const
+bool Matrix::operator==(const Matrix& other) const
+{
+  if (rows() != other.rows() || cols() != other.cols()) {
+    return false;
+  }
+
+
+
+  return false;
+}
+
+bool Matrix::operator!=(const Matrix& other) const
 {
   return false;
 }
 
-bool Matrix::operator!=(const Matrix&) const
-{
-  return false;
-}
-
-Matrix Matrix::operator+(const Matrix&) const
+Matrix Matrix::operator+(const Matrix& other) const
 {
   Matrix a;
   return a;
 }
 
-Matrix Matrix::operator*(const Matrix&) const
+Matrix Matrix::operator*(const Matrix& other) const
 {
   Matrix a;
   return a;
 }
 
-Matrix Matrix::operator*(int) const
+Matrix Matrix::operator*(int factor) const
 {
   Matrix a;
   return a;
--- 1lab/matrix/Matrix.h
+++ 1lab/matrix/Matrix.h
@@ -7,50 +7,50 @@
 class Matrix
 {
  public:
-    typedef unsigned int index;
+    typedef size_t index;
 
-    class matrix_row : private Vector< int >
+    class matrix_row : private Vector<int>
     {
     public:
-        matrix_row(std::size_t s = 0) : Vector< int >( s ) {}
-        using Vector<int>::operator [];
+      matrix_row(std::size_t size = 0) : Vector<int>(size) {}
+      using Vector<int>::operator [];
     private:
-        friend std::istream& operator>>( std::istream&, Matrix& );
+      friend std::istream& operator>>(std::istream&, Matrix&);
     };
 
     Matrix();
     Matrix(const Matrix& other);
-    explicit Matrix(std::size_t size);
-    Matrix(std::size_t rows, std::size_t cols);
+    explicit Matrix(index size);
+    Matrix(index rows, index cols);
     Matrix(std::string serialized);
     ~Matrix();
 
-    Matrix& operator= ( const Matrix& );
-    bool operator== (const Matrix&) const;
-    bool operator!= (const Matrix&) const;
-    Matrix operator+ ( const Matrix& ) const;
-    Matrix operator* ( const Matrix& ) const;
-    Matrix operator* ( int ) const;
-    Matrix operator-( const Matrix& ) const;
-    Matrix operator-( ) const;
+    Matrix& operator= (const Matrix& other);
+    bool operator== (const Matrix& other) const;
+    bool operator!= (const Matrix& other) const;
+    Matrix operator+ (const Matrix& other) const;
+    Matrix operator* (const Matrix& other) const;
+    Matrix operator* (int factor) const;
+    Matrix operator-(const Matrix& other) const;
+    Matrix operator-() const;
 
 
-    Matrix transpose( ) const;
+    Matrix transpose() const;
 
-    matrix_row& operator[]( index i );
-    const matrix_row& operator[]( index i ) const;
+    matrix_row& operator[](index i);
+    const matrix_row& operator[](index i) const;
 
     std::size_t rows() const;
     std::size_t cols() const;
 
  protected:
  private:
-    Vector< matrix_row >        m_vectors;
     std::size_t                 m_rows;
     std::size_t                 m_cols;
-
-    void add_row( );            // Non mandatory help function
-    friend std::istream& operator>> ( std::istream&, Matrix& );
+    Vector<matrix_row>        m_vectors;
+    
+    void add_row();            // Non mandatory help function
+    friend std::istream& operator>> (std::istream&, Matrix&);
 };
 
 std::istream& operator>> ( std::istream& stream, Matrix& matrix);
