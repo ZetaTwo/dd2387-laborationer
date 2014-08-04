48704dd6d5b0852ab6b7085f789394934fdf3bd7
Emil Lundberg <lundberg.emil@gmail.com>
2014-08-05 01:51:02 +0200

    Add Vector<bool>::initial_size(minimumSize)

--- 1lab/vector/kth_cprog_vektor_bool.cpp
+++ 1lab/vector/kth_cprog_vektor_bool.cpp
@@ -78,6 +78,7 @@ private:
 
   void increase_memory(int num_elements, bool copy = true); //Increases memory to fit at least num_elements number of elements
   inline size_t storage_size() const; // How many storage blocks are allocated
+  const size_t initial_size(const size_t& minimumSize) const; // Return a suitable max_size >= minimumSize
 };
 
 //Iterator classes
@@ -458,6 +459,11 @@ void Vector<bool>::increase_memory(int num_elements, bool copy) { //Increases me
 inline size_t Vector<bool>::storage_size() const {
   return max_size / STORAGE_BLOCK_SIZE;
 }
+
+inline const size_t Vector<bool>::initial_size(const size_t& minimumSize) const {
+  return STORAGE_BLOCK_SIZE << static_cast<int>(ceil(log2(std::max(1.0, minimumSize/static_cast<double>(STORAGE_BLOCK_SIZE)))));
+}
+
 
 Vector<bool>::iterator Vector<bool>::begin() {
   return iterator(&data[0], 0);
