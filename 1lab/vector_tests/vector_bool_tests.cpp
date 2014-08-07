af865523283f7edeaba71c6ea336369b95c98333
Emil Lundberg <lundberg.emil@gmail.com>
2014-08-07 20:53:10 +0200

    Refine InsertManyItems parametrization

--- 1lab/vector_tests/vector_bool_tests.cpp
+++ 1lab/vector_tests/vector_bool_tests.cpp
@@ -326,27 +326,27 @@ TEST_P(SizeSizeTest, InsertOneItem) {
   }
 }
 
-TEST_P(SizeSizeTest, InsertManyItems) {
+class InsertManyItemsTest : public TestWithParam<std::tuple<int, int, int> > {};
+INSTANTIATE_TEST_CASE_P(VectorBool, InsertManyItemsTest, Combine(Range(30, 70), Range(0, 30), Range(1, 71)));
+TEST_P(InsertManyItemsTest, InsertManyItems) {
   const size_t size = std::get<0>(GetParam());
   const size_t insert_index = std::get<1>(GetParam());
-  const size_t insert_amount = 70;
+  const size_t insert_amount = std::get<2>(GetParam());
   Vec vector(size);
 
-  if(insert_index <= size) {
-    for(size_t i = 0; i < insert_amount; i++) {
-      vector.insert(insert_index, true);
-    }
+  for(size_t i = 0; i < insert_amount; i++) {
+    vector.insert(insert_index, true);
+  }
 
-    EXPECT_EQ(size + insert_amount, vector.size());
-    for(size_t i = 0; i < insert_index; ++i) {
-      EXPECT_FALSE(vector[i]);
-    }
-    for(size_t i = insert_index; i < insert_index + insert_amount; ++i) {
-      EXPECT_TRUE(vector[i]);
-    }
-    for(size_t i = insert_index + insert_amount; i < vector.size(); ++i) {
-      EXPECT_FALSE(vector[i]);
-    }
+  EXPECT_EQ(size + insert_amount, vector.size());
+  for(size_t i = 0; i < insert_index; ++i) {
+    EXPECT_FALSE(vector[i]);
+  }
+  for(size_t i = insert_index; i < insert_index + insert_amount; ++i) {
+    EXPECT_TRUE(vector[i]);
+  }
+  for(size_t i = insert_index + insert_amount; i < vector.size(); ++i) {
+    EXPECT_FALSE(vector[i]);
   }
 }
 
