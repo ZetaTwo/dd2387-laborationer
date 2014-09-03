be7b586cddc5da739af917b1002329e932c8c2ab
Emil Lundberg <lundberg.emil@gmail.com>
2014-09-03 10:37:09 +0200

    Add cprog14 kurskatalog

---
+++ kurskatalog/lab1/0.4_does_it_fit/count_if_followed_by.h
@@ -0,0 +1,12 @@
+#ifndef COUNT_IF_FOLLWED_BY_H
+
+// .-------------------------------------------------------
+// |        count_if_followed_by (data, len, a, b);
+// |
+// | About: Returns the number of occurances of
+// |        a followed by b in the range [data, data+len).
+// '-------------------------------------------------------
+
+int count_if_followed_by (char const * p, int len, char a, char b);
+
+#endif
