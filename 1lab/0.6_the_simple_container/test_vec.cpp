9f747bae4267920687904aba22a9ae28612c2589
Calle Svensson <calle.svensson@zeta-two.com>
2014-09-24 23:06:33 +0200

    Replaced simple container with typedef to template

--- 1lab/0.6_the_simple_container/test_vec.cpp
+++ 1lab/0.6_the_simple_container/test_vec.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <stdexcept>
-#include "vector.h"     // inkludera din headerfil här
+#include "kth_cprog_simple_container.hpp"     // inkludera din headerfil här
 
 
 int main()
 {
     // Några saker som ska fungera:
-    Vector a(7);           // initiering med 7 element
-    Vector b(a);           // kopieringskonstruktor
-    Vector c = a;          // kopieringskonstruktor
+    UIntVector a(7);           // initiering med 7 element
+    UIntVector b(a);           // kopieringskonstruktor
+    UIntVector c = a;          // kopieringskonstruktor
 
     a = b;                 // tilldelning genom kopiering
     a[5] = 7;              // tilldelning till element
 
-    const Vector e(10);    // konstant objekt med 10 element
+    const UIntVector e(10);    // konstant objekt med 10 element
     int i = e[5];          // const int oper[](int) const körs
     i = a[0];              // vektorn är nollindexerad
     i = a[5];              // int oper[](int) körs
