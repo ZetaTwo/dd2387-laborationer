a71eb1695c0356930ce958eddfe455ac6486b603
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-30 13:42:21 +0100

    Add basic accessor function declarations to Date

--- 2lab/date.h
+++ 2lab/date.h
@@ -1,9 +1,21 @@
 #pragma once
+#include<string>
 
 namespace lab2 {
 
 class Date {
 public:
+
+  virtual int year()            const = 0;
+  virtual int month()           const = 0;
+  virtual int day()             const = 0;
+  virtual int week_day()        const = 0;
+  virtual int days_per_week()   const = 0;
+  virtual int days_this_month() const = 0;
+  virtual int months_per_year() const = 0;
+
+  virtual std::string week_day_name() const = 0;
+  virtual std::string month_name()    const = 0;
 };
 
 }
