d9bc719fb7b842b3bf67cb4cfb7babe9b252b2fe
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-30 16:54:41 +0100

    Add comparison operator declarations to Date

--- 2lab/date.h
+++ 2lab/date.h
@@ -17,6 +17,14 @@ public:
   virtual std::string week_day_name() const = 0;
   virtual std::string month_name()    const = 0;
 
+  int  operator- (const Date& other) const;
+  bool operator==(const Date& other) const;
+  bool operator!=(const Date& other) const;
+  bool operator< (const Date& other) const;
+  bool operator<=(const Date& other) const;
+  bool operator> (const Date& other) const;
+  bool operator>=(const Date& other) const;
+
   virtual Date& operator++()    = 0;
   virtual Date& operator--()    = 0;
   virtual Date& operator+=(int) = 0;
