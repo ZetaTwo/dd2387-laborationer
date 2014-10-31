0551110bdb56c675113105c52457b0048f6f0a9a
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-31 14:38:23 +0100

    Add tests for Date::operator!=(const Date&)

--- 2lab/test/date_test.cpp
+++ 2lab/test/date_test.cpp
@@ -110,3 +110,48 @@ TEST(Date, OperatorEqualsIsTrueForSelf) {
   EXPECT_TRUE(ds == ds);
   EXPECT_TRUE(ds == dsr);
 }
+
+TEST(Date, OperatorNotEqualsReturnsFalseForEqualDates) {
+  const DateStub dsa(0);
+  const DateStub dsb(0);
+  const DateStub& dsar = dsa;
+  const DateStub& dsbr = dsb;
+
+  EXPECT_FALSE(dsa  != dsb);
+  EXPECT_FALSE(dsa  != dsbr);
+  EXPECT_FALSE(dsar != dsb);
+  EXPECT_FALSE(dsar != dsbr);
+}
+
+TEST(Date, OperatorNotEqualsReturnsTrueForUnequalDates) {
+  const DateStub dsa(0);
+  const DateStub dsb(1);
+  const DateStub& dsar = dsa;
+  const DateStub& dsbr = dsb;
+
+  EXPECT_TRUE(dsa  != dsb);
+  EXPECT_TRUE(dsa  != dsbr);
+  EXPECT_TRUE(dsar != dsb);
+  EXPECT_TRUE(dsar != dsbr);
+}
+
+TEST(Date, OperatorNotEqualsIsSymmetricWhenFalse) {
+  const DateStub dsa(0);
+  const DateStub dsb(1);
+
+  EXPECT_EQ(dsa != dsb, dsb != dsa);
+}
+
+TEST(Date, OperatorNotEqualsIsSymmetricWhenTrue) {
+  const DateStub dsa(0);
+  const DateStub dsb(0);
+
+  EXPECT_EQ(dsa != dsb, dsb != dsa);
+}
+
+TEST(Date, OperatorNotEqualsIsFalseForSelf) {
+  const DateStub ds(0);
+  const DateStub& dsr = ds;
+  EXPECT_FALSE(ds != ds);
+  EXPECT_FALSE(ds != dsr);
+}
