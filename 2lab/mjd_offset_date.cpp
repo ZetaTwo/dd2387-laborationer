008fa5631af0837342ed4f02d2e941d0b06f5354
Calle Svensson <calle.svensson@zeta-two.com>
2014-10-31 12:33:42 +0100

    Indentation

--- 2lab/mjd_offset_date.cpp
+++ 2lab/mjd_offset_date.cpp
@@ -2,37 +2,37 @@
 
 namespace lab2 {
 
-MjdOffsetDate::MjdOffsetDate() : mjd_offset(0) {
-}
+  MjdOffsetDate::MjdOffsetDate() : mjd_offset(0) {
+  }
 
-MjdOffsetDate::MjdOffsetDate(int mjd_offset) : mjd_offset(mjd_offset) {
-}
+  MjdOffsetDate::MjdOffsetDate(int mjd_offset) : mjd_offset(mjd_offset) {
+  }
 
-MjdOffsetDate::MjdOffsetDate(const Date& original) : mjd_offset(original.mod_julian_day()) {
-}
+  MjdOffsetDate::MjdOffsetDate(const Date& original) : mjd_offset(original.mod_julian_day()) {
+  }
 
-MjdOffsetDate& MjdOffsetDate::operator=(const Date& other) {
-  mjd_offset = other.mod_julian_day();
-  return *this;
-}
+  MjdOffsetDate& MjdOffsetDate::operator=(const Date& other) {
+    mjd_offset = other.mod_julian_day();
+    return *this;
+  }
 
-MjdOffsetDate& MjdOffsetDate::operator++() {
-  ++mjd_offset;
-  return *this;
-}
+  MjdOffsetDate& MjdOffsetDate::operator++() {
+    ++mjd_offset;
+    return *this;
+  }
 
-MjdOffsetDate& MjdOffsetDate::operator--() {
-  --mjd_offset;
-  return *this;
-}
+  MjdOffsetDate& MjdOffsetDate::operator--() {
+    --mjd_offset;
+    return *this;
+  }
 
-MjdOffsetDate& MjdOffsetDate::operator+=(int days) {
-  mjd_offset += days;
-  return *this;
-}
+  MjdOffsetDate& MjdOffsetDate::operator+=(int days) {
+    mjd_offset += days;
+    return *this;
+  }
 
-MjdOffsetDate& MjdOffsetDate::operator-=(int days) {
-  return operator+=(-days);
-}
+  MjdOffsetDate& MjdOffsetDate::operator-=(int days) {
+    return operator+=(-days);
+  }
 
 }
--- 2lab/mjd_offset_date.h
+++ 2lab/mjd_offset_date.h
@@ -3,23 +3,23 @@
 
 namespace lab2 {
 
-class MjdOffsetDate : public Date {
-protected:
-  int mjd_offset;
+  class MjdOffsetDate : public Date {
+  protected:
+    int mjd_offset;
 
-public:
-  MjdOffsetDate();
-  MjdOffsetDate(int mjd_offset);
-  MjdOffsetDate(const Date& original);
+  public:
+    MjdOffsetDate();
+    MjdOffsetDate(int mjd_offset);
+    MjdOffsetDate(const Date& original);
 
-  virtual MjdOffsetDate& operator=(const Date& other) override;
+    virtual MjdOffsetDate& operator=(const Date& other) override;
 
-  virtual inline int mod_julian_day() const override { return mjd_offset; }
+    virtual inline int mod_julian_day() const override { return mjd_offset; }
 
-  virtual MjdOffsetDate& operator++() override;
-  virtual MjdOffsetDate& operator--() override;
-  virtual MjdOffsetDate& operator+=(int) override;
-  virtual MjdOffsetDate& operator-=(int) override;
-};
+    virtual MjdOffsetDate& operator++() override;
+    virtual MjdOffsetDate& operator--() override;
+    virtual MjdOffsetDate& operator+=(int) override;
+    virtual MjdOffsetDate& operator-=(int) override;
+  };
 
 }
