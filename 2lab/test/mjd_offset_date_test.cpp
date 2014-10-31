a0be3a6098d7995bfd95ac078edac4396c5f2dec
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-31 16:14:56 +0100

    Add test/mjd_offset_date_test.cpp with MjdodStub class

---
+++ 2lab/test/mjd_offset_date_test.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include <sstream>
+#include "mjd_offset_date.h"
+
+using lab2::Date;
+using lab2::MjdOffsetDate;
+
+class MjdodStub : public MjdOffsetDate {
+public:
+  // Methods required for tests
+  using MjdOffsetDate::MjdOffsetDate; // Inherit constructors
+
+  // Methods under test:
+  //  MjdOffsetDate();
+  //  MjdOffsetDate(int mjd_offset);
+  //  MjdOffsetDate(const Date& original);
+  //  virtual MjdOffsetDate& operator=(const Date& other) override;
+  //  virtual inline int mod_julian_day() const override { return mjd_offset; }
+  //  virtual MjdOffsetDate& operator++() override;
+  //  virtual MjdOffsetDate& operator--() override;
+  //  virtual MjdOffsetDate& operator+=(int) override;
+  //  virtual MjdOffsetDate& operator-=(int) override;
+
+  // Dummy implementations for virtual methods
+  virtual int year()            const override { return 0; }
+  virtual int month()           const override { return 0; }
+  virtual int day()             const override { return 0; }
+  virtual int week_day()        const override { return 0; }
+  virtual int days_per_week()   const override { return 0; }
+  virtual int days_this_month() const override { return 0; }
+  virtual int months_per_year() const override { return 0; }
+  virtual std::string week_day_name() const override { return ""; }
+  virtual std::string month_name()    const override { return ""; }
+  virtual Date& add_year (int) override { return *this; }
+  virtual Date& add_month(int) override { return *this; }
+};
