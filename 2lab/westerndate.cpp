e9cd2a39cdb283617b4536de0a8534d00a3f2383
Calle Svensson <calle.svensson@zeta-two.com>
2014-10-31 12:33:34 +0100

    Added Western Date

---
+++ 2lab/westerndate.cpp
@@ -0,0 +1,9 @@
+#include "westerndate.h"
+
+using namespace std;
+
+namespace lab2 {
+  const vector<string> WesternDate::day_names = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
+  const vector<string> WesternDate::month_names = { "january", "february", "march", "april", "may", "june", "july", "august", "spetember", "october", "november", "december" };
+
+}
---
+++ 2lab/westerndate.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "mjd_offset_date.h"
+
+namespace lab2 {
+  class WesternDate : public MjdOffsetDate
+  {
+  public:
+    WesternDate();
+    ~WesternDate();
+
+    virtual int days_per_week() const inline override { return 7; }
+    virtual int months_per_year() const inline override { return 12; }
+
+  protected:
+    static const std::vector<std::string> day_names;
+    static const std::vector<std::string> month_names;
+  };
+}
+
