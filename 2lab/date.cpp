5cca3fe49ac9b6ed9e2a3b52c2844a2c04fca642
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-31 15:52:39 +0100

    Fix Date output operator: Add zero padding

--- 2lab/date.cpp
+++ 2lab/date.cpp
@@ -31,7 +31,16 @@ namespace lab2 {
   }
 
   std::ostream& operator<<(std::ostream& os, const Date& d) {
-    return os << d.year() << "-" << d.month() << "-" << d.day();
+    return os
+      << (d.year() < 1000 ? "0" : "")
+      << (d.year() < 100 ? "0" : "")
+      << (d.year() < 10 ? "0" : "")
+      << d.year()
+      << "-"
+      << (d.month() < 10 ? "0" : "") << d.month()
+      << "-"
+      << (d.day() < 10 ? "0" : "") << d.day()
+      ;
   }
 
 }
