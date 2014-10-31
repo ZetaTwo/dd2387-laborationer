41d55e59ce9619fcb498218b2053897357759620
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-31 13:15:31 +0100

    Verticalize WesternDate vector constants
    For easier reading in windows with text width < 100.

--- 2lab/westerndate.cpp
+++ 2lab/westerndate.cpp
@@ -1,7 +1,28 @@
 #include "westerndate.h"
 
 namespace lab2 {
-  const std::vector<std::string> WesternDate::day_names = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
-  const std::vector<std::string> WesternDate::month_names = { "january", "february", "march", "april", "may", "june", "july", "august", "spetember", "october", "november", "december" };
+  const std::vector<std::string> WesternDate::day_names = {
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  };
+  const std::vector<std::string> WesternDate::month_names = {
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "spetember",
+    "october",
+    "november",
+    "december",
+  };
 
 }
