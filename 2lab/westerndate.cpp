58b8c30b6c535228d0ab6229f91525ebe4c1d5be
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-31 13:09:23 +0100

    Don't using namespace std;

--- 2lab/westerndate.cpp
+++ 2lab/westerndate.cpp
@@ -1,9 +1,7 @@
 #include "westerndate.h"
 
-using namespace std;
-
 namespace lab2 {
-  const vector<string> WesternDate::day_names = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
-  const vector<string> WesternDate::month_names = { "january", "february", "march", "april", "may", "june", "july", "august", "spetember", "october", "november", "december" };
+  const std::vector<std::string> WesternDate::day_names = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
+  const std::vector<std::string> WesternDate::month_names = { "january", "february", "march", "april", "may", "june", "july", "august", "spetember", "october", "november", "december" };
 
 }
