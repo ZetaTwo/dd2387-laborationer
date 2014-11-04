ba16bb6a7c03295e5d69c6a1bbccc637d2245fd7
Emil Lundberg <lundberg.emil@gmail.com>
2014-11-04 17:45:04 +0100

    Add mod_julian_day.py

---
+++ 2lab/mod_julian_day.py
@@ -0,0 +1,7 @@
+from math import floor
+
+def mjd(year, month, day):
+    a = floor((14 - month) / 12)
+    y = year + 4800 - a
+    m = month + 12 * a - 3
+    return day + floor((153 * m + 2) / 5) + 365 * y + floor(y / 4) - floor(y / 100) + floor(y / 400) - 32045 - 2400001
