c85ad65f3b9577723841ee0572737ee6f869fe35
Calle Svensson <calle.svensson@zeta-two.com>
2014-11-04 14:40:06 +0100

    Date output formatting

--- 2lab/date.cpp
+++ 2lab/date.cpp
@@ -1,18 +1,10 @@
 #include "date.h"
 
+#include <iomanip>
+using namespace std;
+
 namespace lab2 {
-
   std::ostream& operator<<(std::ostream& os, const Date& d) {
-    return os
-      << (d.year() < 1000 ? "0" : "")
-      << (d.year() < 100 ? "0" : "")
-      << (d.year() < 10 ? "0" : "")
-      << d.year()
-      << "-"
-      << (d.month() < 10 ? "0" : "") << d.month()
-      << "-"
-      << (d.day() < 10 ? "0" : "") << d.day()
-      ;
+    return os << setw(4) << setfill('0') << d.year() << "-" << setw(2) << d.month() << "-" << d.day();
   }
-
 }
