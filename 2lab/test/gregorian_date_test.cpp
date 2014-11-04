ebf177ddc1531d780d40ed498ee4cf12055716d5
Emil Lundberg <lundberg.emil@gmail.com>
2014-11-05 00:13:51 +0100

    Add tests for GregorianDate::add_month()

--- 2lab/test/gregorian_date_test.cpp
+++ 2lab/test/gregorian_date_test.cpp
@@ -122,6 +122,64 @@ INSTANTIATE_TEST_CASE_P(GregorianDate, InvalidGregorianDateTest, Values(
   std::tuple<int, int, int>{2014, 12, 32}
 ));
 
+class GregorianDateAddMonthTest : public TestWithParam<std::tuple<int, int, int, int, int, int, int>> {};
+INSTANTIATE_TEST_CASE_P(GregorianDate, GregorianDateAddMonthTest, Values(
+  //                                               y0, m0, d0, N,   y1, m1, d1
+  std::tuple<int, int, int, int, int, int, int>{    0,  1,  1, 1,    0,  2,  1 },
+
+  std::tuple<int, int, int, int, int, int, int>{ 1858, 11, 30, 1, 1858, 12, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 1858, 12, 30, 1, 1859,  1, 30 },
+
+  std::tuple<int, int, int, int, int, int, int>{ 1970,  1, 28, 1, 1970,  2, 28 },
+  std::tuple<int, int, int, int, int, int, int>{ 1970,  1, 29, 1, 1970,  2, 28 },
+  std::tuple<int, int, int, int, int, int, int>{ 1970,  1, 30, 1, 1970,  3,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 1970,  1, 31, 1, 1970,  3,  2 },
+
+  std::tuple<int, int, int, int, int, int, int>{ 1972,  1, 28, 1, 1972,  2, 28 },
+  std::tuple<int, int, int, int, int, int, int>{ 1972,  1, 29, 1, 1972,  2, 29 },
+  std::tuple<int, int, int, int, int, int, int>{ 1972,  1, 30, 1, 1972,  2, 29 },
+  std::tuple<int, int, int, int, int, int, int>{ 1972,  1, 31, 1, 1972,  3,  1 },
+
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  1,  1, 1, 2014,  2,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  2,  1, 1, 2014,  3,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  3,  1, 1, 2014,  4,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  4,  1, 1, 2014,  5,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  5,  1, 1, 2014,  6,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  6,  1, 1, 2014,  7,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  7,  1, 1, 2014,  8,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  8,  1, 1, 2014,  9,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  9,  1, 1, 2014, 10,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014, 10,  1, 1, 2014, 11,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014, 11,  1, 1, 2014, 12,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014, 12,  1, 1, 2015,  1,  1 },
+
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  1, 31, 1, 2014,  3,  2 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  2, 28, 1, 2014,  3, 28 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  3, 31, 1, 2014,  4, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  4, 30, 1, 2014,  5, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  5, 31, 1, 2014,  6, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  6, 30, 1, 2014,  7, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  7, 31, 1, 2014,  8, 31 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  8, 31, 1, 2014,  9, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  9, 30, 1, 2014, 10, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014, 10, 31, 1, 2014, 11, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014, 11, 30, 1, 2014, 12, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014, 12, 31, 1, 2015,  1, 31 },
+
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  1, 31,  2, 2014,  4,  2 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  2, 28,  3, 2014,  5, 28 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  3, 31,  4, 2014,  7, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  4, 30,  5, 2014,  9, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  5, 31,  6, 2014, 11, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  6, 30,  7, 2015,  1, 30 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  7, 31,  8, 2015,  4,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  8, 31,  9, 2015,  6,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014,  9, 30, 10, 2015,  8,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014, 10, 31, 11, 2015, 10,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014, 11, 30, 12, 2015, 12,  1 },
+  std::tuple<int, int, int, int, int, int, int>{ 2014, 12, 31, 13, 2016,  2,  2 }
+));
+
 using lab2::Date;
 using lab2::GregorianDate;
 
@@ -252,4 +310,37 @@ TEST_P(ValidGregorianDateTest, OperatorAssignmentDoesNotModifyOriginal) {
   EXPECT_EQ(year, da.year());
   EXPECT_EQ(month, da.month());
   EXPECT_EQ(day, da.day());
+}
+
+TEST_P(GregorianDateAddMonthTest, AddMonthAddsOneMonthIfPossibleOtherwiseAdds30Days) {
+  const int year_before = std::get<0>(GetParam());
+  const int month_before = std::get<1>(GetParam());
+  const int day_before = std::get<2>(GetParam());
+
+  const int num_months = std::get<3>(GetParam());
+
+  const int year_after = std::get<4>(GetParam());
+  const int month_after = std::get<5>(GetParam());
+  const int day_after = std::get<6>(GetParam());
+
+  GregorianDate gda{year_before, month_before, day_before};
+  GregorianDate gdb{year_before, month_before, day_before};
+
+  ASSERT_EQ(year_before, gda.year());
+  ASSERT_EQ(month_before, gda.month());
+  ASSERT_EQ(day_before, gda.day());
+  ASSERT_EQ(gda, gdb);
+
+  gda.add_month(num_months);
+  for(int i = 0; i < num_months; ++i) {
+      gdb.add_month();
+  }
+
+  EXPECT_EQ(year_after, gda.year());
+  EXPECT_EQ(month_after, gda.month());
+  EXPECT_EQ(day_after, gda.day());
+
+  EXPECT_EQ(year_after, gdb.year());
+  EXPECT_EQ(month_after, gdb.month());
+  EXPECT_EQ(day_after, gdb.day());
 }
