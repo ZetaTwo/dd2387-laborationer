3623ac565f20722577082a3c6530e0d5f74effc1
Emil Lundberg <lundberg.emil@gmail.com>
2014-11-05 19:48:17 +0100

    Add private members to Calendar header

--- 2lab/calendar.h
+++ 2lab/calendar.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <ostream>
+#include <map>
+#include <set>
 #include "date.h"
 
 namespace lab2 {
@@ -9,6 +11,11 @@ namespace lab2 {
   class Calendar {
     private:
       static const int UNSPECIFIED_EVENT_DATE_PART = -1;
+      typedef std::string Event;
+      typedef std::map<const D, std::set<Event>> EventCollection;
+
+      D current_date;
+      EventCollection events;
 
     public:
       Calendar();
@@ -18,11 +25,11 @@ namespace lab2 {
 
       bool set_date(int year, int month, int day);
 
-      bool add_event(std::string event,
+      bool add_event(const Event& event,
         int day   = UNSPECIFIED_EVENT_DATE_PART,
         int month = UNSPECIFIED_EVENT_DATE_PART,
         int year  = UNSPECIFIED_EVENT_DATE_PART);
-      bool remove_event(std::string event,
+      bool remove_event(const Event& event,
         int day   = UNSPECIFIED_EVENT_DATE_PART,
         int month = UNSPECIFIED_EVENT_DATE_PART,
         int year  = UNSPECIFIED_EVENT_DATE_PART);
