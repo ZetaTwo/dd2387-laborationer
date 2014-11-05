dc4ce657fc9bab30f12eead33f90d6f1fa250dcb
Emil Lundberg <lundberg.emil@gmail.com>
2014-11-05 15:23:11 +0100

    Add docstrings to functions in mod_julian_day.py

--- 2lab/mod_julian_day.py
+++ 2lab/mod_julian_day.py
@@ -4,12 +4,14 @@ from math import floor
 from sys import argv, exit
 
 def mjd(year, month, day):
+    """Compute the modified Julian day number of the given day in the Gregorian calendar"""
     a = floor((14 - month) / 12)
     y = year + 4800 - a
     m = month + 12 * a - 3
     return day + floor((153 * m + 2) / 5) + 365 * y + floor(y / 4) - floor(y / 100) + floor(y / 400) - 32045 - 2400001
 
 def mjd_jul(year, month, day) :
+    """Compute the modified Julian day number of the given day in the Julian calendar"""
     a = floor((14 - month) / 12)
     y = year + 4800 - a
     m = month + 12 * a - 3
