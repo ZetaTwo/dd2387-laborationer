9f528f27cf2e3c326becd0569d78174fdea0cf1e
Emil Lundberg <lundberg.emil@gmail.com>
2014-11-05 12:53:06 +0100

    Group pure virtual method declarations together in WesternDate

--- 2lab/western_date.h
+++ 2lab/western_date.h
@@ -37,12 +37,11 @@ namespace lab2 {
     static const std::vector<int> month_lengths;
 
     inline bool is_leap_year() const { return is_leap_year(year()); }
-    virtual bool is_leap_year(int year) const = 0;
-
     inline bool is_valid_date(int year, int month, int day) const {
       return month <= 12 && month >= 1 && day >= 1 && day <= (month_lengths[month-1] + (month == 2 && is_leap_year(year) ? 1 : 0));
     }
 
+    virtual bool is_leap_year(int year) const = 0;
     virtual int ymd_to_mjd_offset(int year, int month, int day) const = 0;
 
     struct DateDecomposition {
