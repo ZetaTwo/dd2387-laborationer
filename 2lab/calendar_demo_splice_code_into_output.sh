58c3481aeeab7e5693f08888357a55b0b2c16630
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-02 20:20:23 +0100

    Include semicolon in spliced echo output

--- 2lab/calendar_demo_splice_code_into_output.sh
+++ 2lab/calendar_demo_splice_code_into_output.sh
@@ -5,12 +5,12 @@
 # {     : Begin a block of commands
 # h     : Copy the matched line to hold space
 # s/^\(\s\+\)\([^;]*\)/ : Extract two parts of the line: 1: the leading
-#         whitespace; 2: the following text until the first semicolon
+#         whitespace; 2: the following text
 # \1std::cout << "\2" << std::endl : And insert std::cout<<" before and "<<std::endl around the instruction
 # G     : Append hold space to pattern space (which now contains our replaced line)
 # }     : End the block of commands
 sed '/^  /{
 h;
-s/^\(\s\+\)\([^;]*\)/\1std::cout << "\2" << std::endl/;
+s/^\(\s\+\)\(.*\)/\1std::cout << "\2" << std::endl;/;
 G;
 }' calendar_demo.cpp
