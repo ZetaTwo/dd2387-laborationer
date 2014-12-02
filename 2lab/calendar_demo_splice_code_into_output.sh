1073ac70ef19b5669ba88e43d4953354f03cb0d6
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-02 20:32:58 +0100

    Escape strings in spliced echo program

--- 2lab/calendar_demo_splice_code_into_output.sh
+++ 2lab/calendar_demo_splice_code_into_output.sh
@@ -4,6 +4,7 @@
 # /^  / : Find lines that begin with two spaces
 # {     : Begin a block of commands
 # h     : Copy the matched line to hold space
+# s/"/\\"/g; : Replace all occurrences of " with \"
 # s/^\(\s\+\)\([^;]*\)/ : Extract two parts of the line: 1: the leading
 #         whitespace; 2: the following text
 # \1std::cout << "\2" << std::endl : And insert std::cout<<" before and "<<std::endl around the instruction
@@ -11,6 +12,7 @@
 # }     : End the block of commands
 sed '/^  /{
 h;
+s/"/\\"/g;
 s/^\(\s\+\)\(.*\)/\1std::cout << "\2" << std::endl;/;
 G;
 }' calendar_demo.cpp
