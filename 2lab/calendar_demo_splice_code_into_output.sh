31e68b8545f7b7d06a36ac7ac07d024b87c03432
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-02 20:19:34 +0100

    Make splice script make the new program echo first and then execute

--- 2lab/calendar_demo_splice_code_into_output.sh
+++ 2lab/calendar_demo_splice_code_into_output.sh
@@ -3,13 +3,14 @@
 
 # /^  / : Find lines that begin with two spaces
 # {     : Begin a block of commands
-# p     : Print the matched line
-# ;     : And perform another command...
+# h     : Copy the matched line to hold space
 # s/^\(\s\+\)\([^;]*\)/ : Extract two parts of the line: 1: the leading
 #         whitespace; 2: the following text until the first semicolon
 # \1std::cout << "\2" << std::endl : And insert std::cout<<" before and "<<std::endl around the instruction
+# G     : Append hold space to pattern space (which now contains our replaced line)
 # }     : End the block of commands
 sed '/^  /{
-p;
+h;
 s/^\(\s\+\)\([^;]*\)/\1std::cout << "\2" << std::endl/;
+G;
 }' calendar_demo.cpp
