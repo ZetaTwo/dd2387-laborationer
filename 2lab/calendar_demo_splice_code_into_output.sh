f04f925d6baa0f3a976cb3c5bfc65c0839c7d30f
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-02 20:34:50 +0100

    Prefix instruction lines with ' > ' in spliced echo output

--- 2lab/calendar_demo_splice_code_into_output.sh
+++ 2lab/calendar_demo_splice_code_into_output.sh
@@ -7,12 +7,13 @@
 # s/"/\\"/g; : Replace all occurrences of " with \"
 # s/^\(\s\+\)\([^;]*\)/ : Extract two parts of the line: 1: the leading
 #         whitespace; 2: the following text
-# \1std::cout << "\2" << std::endl : And insert std::cout<<" before and "<<std::endl around the instruction
+# \1std::cout << " > \2" << std::endl : And insert std::cout<<" before and "<<std::endl around
+#         the instruction, and prefix the instruction line with " > " in the output
 # G     : Append hold space to pattern space (which now contains our replaced line)
 # }     : End the block of commands
 sed '/^  /{
 h;
 s/"/\\"/g;
-s/^\(\s\+\)\(.*\)/\1std::cout << "\2" << std::endl;/;
+s/^\(\s\+\)\(.*\)/\1std::cout << " > \2" << std::endl;/;
 G;
 }' calendar_demo.cpp
