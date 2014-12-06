9fafab701bbdd558777f03085b9620d9bb7e6872
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-06 21:08:54 +0100

    Add more multiplication test cases to buggy matrix tests

--- 1lab/1.1_the_matrix/buggies/matrix_tests.cpp
+++ 1lab/1.1_the_matrix/buggies/matrix_tests.cpp
@@ -48,6 +48,10 @@ INSTANTIATE_TEST_CASE_P(Matrix, AdditionTest, Values(
 class MultiplicationTest : public TestWithParam<tuple<string, string, string>> {};
 INSTANTIATE_TEST_CASE_P(Matrix, MultiplicationTest, Values(
   tuple<string, string, string>{"[ 1 2 ; 3 4 ]", "[ 0 0 ; 0 0 ]", "[ 0 0 ; 0 0 ]"},
+  tuple<string, string, string>{"[ 1 2 ; 3 4 ]", "[ 1 0 ; 0 1 ]", "[ 1 2 ; 3 4 ]"},
+  tuple<string, string, string>{"[ 1 2 ; 3 4 ]", "[ -1 0 ; 0 -1 ]", "[ -1 -2 ; -3 -4 ]"},
+  tuple<string, string, string>{"[ 1 2 ; 3 4 ]", "[ 0 1 ; 1 0 ]", "[ 2 1 ; 4 3 ]"},
+  tuple<string, string, string>{"[ 1 2 ; 3 4 ]", "[ 0 -1 ; -1 0 ]", "[ -2 -1 ; -4 -3 ]"},
   tuple<string, string, string>{"[ 1 2 3 ; 4 5 6 ]", "[ 7 8 ; 9 10; 11 12 ]", "[ 58 64; 139 154 ]"}
 ));
 
