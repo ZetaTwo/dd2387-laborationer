034795815a92e8fa6218b7a743c5ba73dc90621c
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-11 15:08:19 +0100

    Make Hypercube superclass private, expose only operator[]

--- 1lab/1.4_the_hypercube/hypercube.hpp
+++ 1lab/1.4_the_hypercube/hypercube.hpp
@@ -1,15 +1,19 @@
 #include "kth_cprog_template_container.hpp"
 
 template<class T, size_t n>
-class Hypercube : public Vector<Hypercube<T, n-1> > {
+class Hypercube : Vector<Hypercube<T, n-1> > {
 public:
   Hypercube() : Vector<Hypercube<T, n-1> >() {};
   Hypercube(size_t s) : Vector<Hypercube<T, n-1> >(s, Hypercube<T, n-1>(s)) {};
+
+  using Vector<Hypercube<T, n-1>>::operator[];
 };
 
 template<class T>
-class Hypercube<T, 1> : public Vector<T> {
+class Hypercube<T, 1> : Vector<T> {
 public:
   Hypercube() : Vector<T>() {};
   Hypercube(size_t s) : Vector<T>(s) {};
+
+  using Vector<T>::operator[];
 };
