d0512ce3a163739fd64980d89a6368d3b47df62f
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-12 23:25:05 +0100

    Add more syntax tests for Vector<bool>::iterator

--- 1lab/1.5_space_is_not_infinite/vector_bool_syntax_tests.cpp
+++ 1lab/1.5_space_is_not_infinite/vector_bool_syntax_tests.cpp
@@ -2,6 +2,8 @@
 #include "kth_cprog_vektor_bool.hpp"
 
 using std::iterator_traits;
+using std::move;
+using std::swap;
 using ::testing::Test;
 
 typedef Vector<bool> Vec;
@@ -9,6 +11,7 @@ typedef Vec::iterator It;
 typedef Vec::const_iterator CIt;
 typedef iterator_traits<It>::difference_type difference;
 typedef iterator_traits<It>::reference reference;
+typedef iterator_traits<It>::value_type value_type;
 
 struct VectorBoolIterator : Test {
     Vec v;
@@ -103,4 +106,79 @@ TEST_F(VectorBoolIterator, IsPostfixIncrementable) {
 
 TEST_F(VectorBoolIterator, IsPostfixIncrementDereferenceable) {
   reference it = *b++;
+}
+
+// InputIterator
+
+TEST_F(VectorBoolIterator, IsNotEqualComparable) {
+  bool a = b != e;
+}
+
+TEST_F(VectorBoolIterator, IsAsteriskDereferenceable) {
+  value_type v = *b;
+}
+
+// operator-> is not applicable: bool a; a->foo is nonsensical
+
+// DefaultConstructible
+
+TEST_F(VectorBoolIterator, IsDefaultInitializable) {
+  It it;
+}
+
+TEST_F(VectorBoolIterator, IsDefaultValueInitializable) {
+  It it{};
+}
+
+TEST_F(VectorBoolIterator, IsDefaultTemporaryValueInitializable) {
+  It();
+  It{};
+}
+
+// CopyConstructible
+
+TEST_F(VectorBoolIterator, IsCopyConstructible) {
+  It it1 = b;
+  It it2(b);
+  It it3{b};
+}
+
+// MoveConstructible
+
+TEST_F(VectorBoolIterator, IsMoveConstructible) {
+  It it1 = move(It{});
+  It it2(move(It{}));
+  It it3{move(It{})};
+}
+
+// CopyAssignable
+
+TEST_F(VectorBoolIterator, IsCopyAssignable) {
+  It it;
+  it = b;
+}
+
+// MoveAssignable
+
+TEST_F(VectorBoolIterator, IsMoveAssignable) {
+  It it;
+  it = std::move(It{});
+}
+
+// Destructible
+
+TEST_F(VectorBoolIterator, IsDestructible) {
+  b.~It();
+}
+
+// Swappable
+
+TEST_F(VectorBoolIterator, IsSwappable) {
+  swap(b, e);
+}
+
+// EqualityComparable
+
+TEST_F(VectorBoolIterator, IsEqualityComparable) {
+  bool a = b == e;
 }
