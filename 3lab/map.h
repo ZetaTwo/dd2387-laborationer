00e759fbc3aa6bc45840e65d8f00cb1f443506ea
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-15 20:01:11 +0100

    Remove get_tile(int, int) methods from Map

--- 3lab/map.h
+++ 3lab/map.h
@@ -20,9 +20,7 @@ namespace lab3 {
       Map(vector<vector<shared_ptr<Tile>>> tiles);
 
       const Tile& get_tile(const Coord& coord) const;
-      const Tile& get_tile(int x, int y) const;
       Tile& get_tile(const Coord& coord);
-      Tile& get_tile(int x, int y);
 
       void tick();
   };
