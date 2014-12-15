ffde33672e5036c541c2bb9157c5ec0d5f206d4d
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-15 20:01:45 +0100

    Add more method declarations to Map header

--- 3lab/map.h
+++ 3lab/map.h
@@ -19,8 +19,20 @@ namespace lab3 {
     public:
       Map(vector<vector<shared_ptr<Tile>>> tiles);
 
+      inline Coord top_left() const { return Coord{0, 0}; }
+      Coord btm_right() const;
+      inline CoordRectangle range() const { return CoordRectangle{top_left(), btm_right()}; }
+
       const Tile& get_tile(const Coord& coord) const;
       Tile& get_tile(const Coord& coord);
+
+      bool set_tile                (const Coord& coord, shared_ptr<Tile> tile_p);
+      bool insert_tile_north_of    (const Coord& coord, shared_ptr<Tile> tile_p);
+      bool insert_tile_west_of     (const Coord& coord, shared_ptr<Tile> tile_p);
+      bool push_back_tile_south_of (const Coord& coord, shared_ptr<Tile> tile_p);
+      bool push_back_tile_east_of  (const Coord& coord, shared_ptr<Tile> tile_p);
+      bool remove_tile_shift_north (const Coord& coord, shared_ptr<Tile> tile_p);
+      bool remove_tile_shift_west  (const Coord& coord, shared_ptr<Tile> tile_p);
 
       void tick();
   };
