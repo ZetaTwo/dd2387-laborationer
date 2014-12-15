c807377a6837c3d82bc5aaad541eaf719589a711
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-15 20:44:43 +0100

    Implement Map::set_tile

--- 3lab/map.cpp
+++ 3lab/map.cpp
@@ -38,6 +38,23 @@ namespace lab3 {
     };
   }
 
+  bool Map::set_tile(const Coord& coord, tile_ptr_t tile_p) {
+    while(rows.size() <= coord.y) {
+      rows.push_back(row_t{});
+    }
+    while(rows[coord.y].size() < coord.x) {
+      rows[coord.y].push_back(default_tile_p);
+    }
+
+    if(rows[coord.y].size() == coord.x) {
+      rows[coord.y].push_back(tile_p);
+    } else {
+      rows[coord.y][coord.x] = tile_p;
+    }
+
+    return true;
+  }
+
   ostream& operator<<(ostream& os, const Map& map) {
     size_t prev_row = 0;
     for(const Coord& xy : map.range()) {
