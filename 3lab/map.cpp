32fef7875be7a7dbc5c8c58d9313b9f4ddac5b03
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-15 20:12:47 +0100

    Implement operator<< for Map

--- 3lab/map.cpp
+++ 3lab/map.cpp
@@ -2,6 +2,7 @@
 
 #include "map.h"
 
+using std::endl;
 using std::max_element;
 
 namespace lab3 {
@@ -24,6 +25,19 @@ namespace lab3 {
       max_x_row_it == rows.end() ? 0 : max_x_row_it->size() == 0 ? 0 : max_x_row_it->size() - 1,
       rows.size() - 1
     };
+  }
+
+  ostream& operator<<(ostream& os, const Map& map) {
+    size_t prev_row = 0;
+    for(const Coord& xy : map.range()) {
+      if(xy.y > prev_row) {
+        prev_row = xy.y;
+        os << endl;
+      }
+      os << xy << "\t";
+    }
+
+    return os;
   }
 
 }
