fa9a53c78574ac622c4d3a9db8b3fa89536891a3
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-16 16:13:10 +0100

    Print some mock output in Renderer::render

--- 3lab/renderer.cpp
+++ 3lab/renderer.cpp
@@ -11,6 +11,18 @@ namespace lab3 {
     out(out) {}
 
   void Renderer::render(const Game& game) {
+    const Player& player = game.get_player();
+    const Actor& player_actor = player.get_actor();
+
+    const WorldCoord& player_position = player_actor.get_position();
+
+    const Map& player_map = game.get_world().get_map(player_position.map_id);
+
+    out << "First map in world:" << endl;
+    out << game.get_world().get_maps().begin()->second << endl;
+
+    out << "Player map:" << endl;
+    out << player_map << endl;
   };
 
 }
