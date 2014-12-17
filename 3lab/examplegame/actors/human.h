4c916601395dea04a2d0450d396f26ff873cc726
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-18 00:29:16 +0100

    Re-add forgotten human.* files

---
+++ 3lab/examplegame/actors/human.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#include "actor.h"
+#include "inputer.h"
+
+using std::string;
+using std::vector;
+
+namespace lab3 {
+
+  class Human : public Actor {
+  private:
+    string name;
+    Inputer::command_t last_command;
+
+    protected:
+      virtual void do_tick(Game& game) override;
+
+    public:
+      Human(const WorldCoord& initial_position);
+      Human(const WorldCoord& initial_position, const string& name);
+
+      virtual inline bool is_solid() const { return true; }
+      virtual inline bool is_visible() const { return true; }
+
+      virtual void activated_by(Entity& activator) override;
+
+      static const vector<string> FIRST_NAMES;
+      static const vector<string> LAST_NAMES;
+
+      virtual void input(Game& input) override;
+
+      static string new_name();
+      virtual inline string get_name() const override { return name; }
+      virtual inline string get_description() const override  { return ""; }
+  };
+
+}
