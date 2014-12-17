0f96d54604d43e2f88be9caf16efa6228b9d167a
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-17 03:14:29 +0100

    Remove unused includes from test.cpp

--- 3lab/test.cpp
+++ 3lab/test.cpp
@@ -1,20 +1,6 @@
-#include <iostream>
-#include <memory>
-#include <vector>
-
-#include "actors/basic.h"
 #include "game.h"
-#include "map.h"
-#include "tile.h"
-#include "tiles/hazards.h"
-#include "renderer.h"
 
 using namespace lab3;
-using std::make_shared;
-using std::vector;
-
-using std::cout;
-using std::endl;
 
 int main() {
   Game& g = *Game::get_instance();
