975891bfd5e45f0c9f278800f644f01d5b521450
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-18 07:22:47 +0100

    Add dialogue to old man

--- 3lab/examplegame/actors/oldman.cpp
+++ 3lab/examplegame/actors/oldman.cpp
@@ -8,6 +8,20 @@ namespace lab3 {
   OldMan::OldMan(const WorldCoord& initial_position, const string& name) : Human(initial_position, name) {}
 
   void OldMan::activated_by(Game& game, Actor& activator) {
+    stringstream ss;
+
+    ss << get_name() << " says:" << endl;
+
+    switch(inventory.size()) {
+      case 0:
+        ss << "Have you seen my favourite shiny thing?" << endl;
+        break;
+      case 1:
+        ss << "Thank you for finding my favourite shiny thing!" << endl;
+        break;
+    }
+
+    game.push_message(ss.str());
   }
 
 
