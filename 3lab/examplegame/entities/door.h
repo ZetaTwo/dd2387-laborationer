cb875f332224189608c35d928ad00f799d13379f
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-18 13:38:58 +0100

    Make door openable/closeable by activating

--- 3lab/examplegame/entities/door.h
+++ 3lab/examplegame/entities/door.h
@@ -12,6 +12,8 @@ namespace lab3 {
     virtual inline string get_name() const override { return "Door"; }
     virtual inline string get_description() const override  { return ""; }
 
+    virtual void activated_by(Game& game, Actor& activator);
+
     virtual bool is_solid()   const override { return !open; }
     virtual bool is_visible() const override { return true; }
     virtual char to_char()    const override {
