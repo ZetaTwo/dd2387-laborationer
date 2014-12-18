49c060bb9aaa7c63de46d648dfef9ef3e679ec85
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-18 02:50:40 +0100

    Re-order things in player.h

--- 3lab/examplegame/actors/player.h
+++ 3lab/examplegame/actors/player.h
@@ -5,18 +5,20 @@
 namespace lab3 {
 
   class Player : public Human {
+    Inputer::command_t last_command;
+
   public:
     Player(const WorldCoord& initial_position) : Human(initial_position, "You") {}
 
-    Inputer::validation_result_t commands_help(const Inputer::command_t& command) const;
     Inputer::validation_result_t validate_command(const Inputer::command_t& command) const;
-    Inputer::validation_result_t validate_command_directional(const Inputer::command_t& command) const;
 
   protected:
     virtual void input(Game& input) override;
 
   private:
-    Inputer::command_t last_command;
+    Inputer::validation_result_t commands_help(const Inputer::command_t& command) const;
+    Inputer::validation_result_t validate_command_directional(const Inputer::command_t& command) const;
+
   };
 
 }
