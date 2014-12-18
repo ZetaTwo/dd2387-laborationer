6c25db8d72ebcc33f7c9c7a01785c853adb22655
Calle Svensson <calle.svensson@zeta-two.com>
2014-12-18 04:12:34 +0100

    Started adding content to world

--- 3lab/examplegame/examplegame.cpp
+++ 3lab/examplegame/examplegame.cpp
@@ -1,5 +1,6 @@
 #include "examplegame.h"
-#include "actors/human.h"
+#include "actors/oldman.h"
+#include "actors/evilking.h"
 #include "actors/player.h"
 #include "tiles/fire.h"
 #include "tiles/rock.h"
@@ -12,40 +13,68 @@ namespace lab3 {
     if (initialized) {
       throw std::runtime_error("Game has already been initialized!");
     }
+    initialized = true;
 
+    start_message();
+
+    Identifiable::identifier_t map_id = create_map();
+
+    shared_ptr<PhysicalEntity> player = make_shared<Player>(WorldCoord{ map_id, 3, 10 });
+    camera.set_followee(player);
+ 
+    world.add_physical(player);
+    world.add_physical(make_shared<OldMan>(WorldCoord{ map_id, 5, 4 }));
+    world.add_physical(make_shared<EvilKing>(WorldCoord{ map_id, 28, 10 }));
+  }
+
+  void ExampleGame::start_message() const {
     cout << "Welcome!" << endl;
     cout << "There is an evil king ruling over the kingdom. " << endl;
     cout << "Yada yada yada. <Generic fantasy RPG background here>." << endl;
     cout << endl;
     cout << "View available commands with \"help\"." << endl;
     cout << "Press enter to start." << endl;
-
     cin.get();
+  }
 
-    initialized = true;
-
+  Identifiable::identifier_t ExampleGame::create_map() {
     Map m;
 
-    m.set_tile(Coord{ 3, 4 }, make_shared<FireTile>());
-    m.set_tile(Coord{ 2, 1 }, make_shared<FireTile>());
-    m.set_tile(Coord{ 5, 3 }, make_shared<FireTile>());
-
+    //Walls
     for (Coord::coord_t x = 0; x < 30; ++x) {
-      m.set_tile(Coord{x, 0}, make_shared<RockTile>());
-      m.set_tile(Coord{x, 15}, make_shared<RockTile>());
+      m.set_tile(Coord{ x, 0 }, make_shared<RockTile>());
+      m.set_tile(Coord{ x, 15 }, make_shared<RockTile>());
     }
-    for(Coord::coord_t y = 1; y < 15; ++y) {
-      m.set_tile(Coord{0, y}, make_shared<RockTile>());
-      m.set_tile(Coord{30, y}, make_shared<RockTile>());
+    for (Coord::coord_t y = 1; y < 15; ++y) {
+      m.set_tile(Coord{ 0, y }, make_shared<RockTile>());
+      m.set_tile(Coord{ 30, y }, make_shared<RockTile>());
+
+      if (y != 8) {
+        m.set_tile(Coord{ 20, y }, make_shared<RockTile>());
+      }
+      else {
+        //m.set_tile(Coord{ 20, y }, make_shared<MagicDoorTile>());
+      }
     }
 
+    //House
+    m.set_tile(Coord{ 3, 3 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 4, 3 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 5, 3 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 6, 3 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 7, 3 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 3, 6 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 4, 6 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 6, 6 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 7, 6 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 3, 4 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 3, 5 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 7, 4 }, make_shared<RockTile>());
+    m.set_tile(Coord{ 7, 5 }, make_shared<RockTile>());
+
+    Identifiable::identifier_t id = m.get_id();
     world.add_map(std::move(m));
 
-    shared_ptr<PhysicalEntity> player = make_shared<Player>(WorldCoord{ m.get_id(), 3, 4 });
-    camera.set_followee(player);
- 
-    world.add_physical(player);
-    world.add_physical(make_shared<Human>(WorldCoord{ m.get_id(), 10, 7 }));
-    world.add_physical(make_shared<Human>(WorldCoord{ m.get_id(), 20, 3 }));
+    return id;
   }
 }
--- 3lab/examplegame/examplegame.h
+++ 3lab/examplegame/examplegame.h
@@ -6,6 +6,7 @@ namespace lab3 {
 
   class ExampleGame : public Game {
     virtual void initialize() override;
-
+    void start_message() const;
+    Identifiable::identifier_t create_map();
   };
 }
