e41ca58207b03b9f9dd542262a736350303e266e
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-18 02:13:54 +0100

    Use member function pointers to validate commands

--- 3lab/examplegame/actors/player.cpp
+++ 3lab/examplegame/actors/player.cpp
@@ -16,6 +16,13 @@ namespace lab3 {
     { "l", DIR_WEST }
   };
 
+  const map<string, Inputer::validation_result_t (Player::*) (const Inputer::command_t& subcommand) const> TOP_COMMANDS = {
+    { "g", &Player::validate_subcommand_move },
+    { "go", &Player::validate_subcommand_move },
+    { "m", &Player::validate_subcommand_move },
+    { "move", &Player::validate_subcommand_move },
+  };
+
   void Player::input(Game& game) {
     last_command = game.get_inputer().get_input(game, *this);
   }
@@ -29,17 +36,25 @@ namespace lab3 {
       return { false, "Commands:\ng (u | d | l | r) : Move up, down, left, right respectively" };
     }
 
+    auto validator_it = TOP_COMMANDS.find(command[0]);
+    if(validator_it != TOP_COMMANDS.end()) {
+      return (this->*(validator_it->second))(command);
+    }
+
     if(command[0] == "g") {
-      if(command.size() > 1) {
-        if(DIRECTION_COMMANDS.count(command[1]) > 0) {
-          return { true, "" };
-        }
-        return { false, "Invalid direction." };
-      }
-      return { false, "No direction specified." };
     }
 
     return { false, "Unrecognized command." };
+  }
+
+  Inputer::validation_result_t Player::validate_subcommand_move(const Inputer::command_t& command) const {
+    if(command.size() > 1) {
+      if(DIRECTION_COMMANDS.count(command[1]) > 0) {
+        return { true, "" };
+      }
+      return { false, "Invalid direction." };
+    }
+    return { false, "No direction specified." };
   }
 
 }
--- 3lab/examplegame/actors/player.h
+++ 3lab/examplegame/actors/player.h
@@ -9,6 +9,7 @@ namespace lab3 {
     Player(const WorldCoord& initial_position) : Human(initial_position, "You") {}
 
     Inputer::validation_result_t validate_command(const Inputer::command_t& command) const;
+    Inputer::validation_result_t validate_subcommand_move(const Inputer::command_t& command) const;
 
   protected:
     virtual void input(Game& input) override;
