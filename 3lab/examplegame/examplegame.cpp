8a64d2b9a1ddea3ecb02ab81428a4d458ce0e895
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-18 03:41:00 +0100

    Add a rectangle of rock around the starting map

--- 3lab/examplegame/examplegame.cpp
+++ 3lab/examplegame/examplegame.cpp
@@ -2,6 +2,7 @@
 #include "actors/human.h"
 #include "actors/player.h"
 #include "tiles/fire.h"
+#include "tiles/rock.h"
 
 using std::make_shared;
 
@@ -19,6 +20,15 @@ namespace lab3 {
     m.set_tile(Coord{ 3, 4 }, make_shared<FireTile>());
     m.set_tile(Coord{ 2, 1 }, make_shared<FireTile>());
     m.set_tile(Coord{ 5, 3 }, make_shared<FireTile>());
+
+    for(int x = 0; x < 30; ++x) {
+      m.set_tile(Coord{x, 0}, make_shared<RockTile>());
+      m.set_tile(Coord{x, 15}, make_shared<RockTile>());
+    }
+    for(int y = 1; y < 15; ++y) {
+      m.set_tile(Coord{0, y}, make_shared<RockTile>());
+      m.set_tile(Coord{30, y}, make_shared<RockTile>());
+    }
 
     world.add_map(std::move(m));
 
