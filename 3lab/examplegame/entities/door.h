1f65e8cb1646df7d5e7f58cb17ea6bd94dce0a3e
Emil Lundberg <lundberg.emil@gmail.com>
2014-12-18 07:26:05 +0100

    Implement state in Door

--- 3lab/examplegame/entities/door.h
+++ 3lab/examplegame/entities/door.h
@@ -7,12 +7,12 @@ namespace lab3 {
     bool open;
 
   public:
-    Door(const WorldCoord& initial_position) : PhysicalEntity(initial_position) {};
+    Door(const WorldCoord& initial_position) : PhysicalEntity(initial_position), open(false) {};
 
     virtual inline string get_name() const override { return "Door"; }
     virtual inline string get_description() const override  { return ""; }
 
-    virtual bool is_solid()   const override { return open; }
+    virtual bool is_solid()   const override { return !open; }
     virtual bool is_visible() const override { return true; }
     virtual char to_char()    const override {
       return 'X';
