50cfef4c2f7de36f0989b263e75bd95ac6497af8
Emil Lundberg <lundberg.emil@gmail.com>
2015-01-06 17:47:23 +0100

    Add ThreadSwapTestWithHugeType

--- 1lab/1.3_concurrency_is_the_new_black/vector_thread_tests.cpp
+++ 1lab/1.3_concurrency_is_the_new_black/vector_thread_tests.cpp
@@ -4,6 +4,26 @@
 //#define LOCK_GLOBAL
 #include "kth_cprog_safe_vector.hpp"
 
+template<size_t n>
+struct LargeType {
+  LargeType<n - 1> a, b;
+  LargeType() : LargeType(0) {}
+  LargeType(long long int a) : a(a), b(a) {}
+  inline bool operator==(const long long int a) const { return this->a == a; }
+  inline bool operator==(const LargeType& other) const { return a == other.a; }
+};
+template<>
+struct LargeType<0> {
+  long long int a;
+  LargeType() : LargeType(0) {}
+  LargeType(long long int a) : a(a) {}
+  inline bool operator==(const long long int a) const { return this->a == a; }
+  inline bool operator==(const LargeType& other) const { return a == other.a; }
+};
+
+typedef LargeType<7> Kibibyte;
+typedef LargeType<17> Mebibyte;
+typedef LargeType<27> Gibibyte;
 
 template<typename T>
 void swapn(SafeVector<T>& vec, size_t i1, size_t i2, int n)
@@ -47,3 +67,24 @@ TEST(SafeVector, ThreadSwapTest) {
   EXPECT_EQ(3, safe1[3]);
 }
 
+TEST(SafeVector, ThreadSwapTestWithHugeType) {
+  typedef LargeType<14> HugeType;
+  SafeVector<HugeType> safe1{ 1, 2, 3, 4 };
+
+  const int n = 10000;
+
+  std::thread thread1(swapn<HugeType>, std::ref(safe1), 0, 1, n);
+  std::thread thread2(swapn<HugeType>, std::ref(safe1), 1, 0, n + 1);
+  std::thread thread3(swapn<HugeType>, std::ref(safe1), 2, 3, n);
+  std::thread thread4(swapn<HugeType>, std::ref(safe1), 3, 2, n - 1);
+
+  thread1.join();
+  thread2.join();
+  thread3.join();
+  thread4.join();
+
+  EXPECT_EQ(2, safe1[0]);
+  EXPECT_EQ(1, safe1[1]);
+  EXPECT_EQ(4, safe1[2]);
+  EXPECT_EQ(3, safe1[3]);
+}
