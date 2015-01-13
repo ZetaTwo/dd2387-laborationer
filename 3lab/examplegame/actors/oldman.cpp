e1343877bc4eebad69fda5584fd4f7336c8ac964
Emil Lundberg <lundberg.emil@gmail.com>
2015-01-13 20:46:01 +0100

    Make OldMan spawn goblins when receiving shiny thing, and react to them

--- 3lab/examplegame/actors/oldman.cpp
+++ 3lab/examplegame/actors/oldman.cpp
@@ -1,12 +1,21 @@
+#include <algorithm>
+#include <memory>
+
 #include "actors/oldman.h"
+#include "entities/door.h"
+#include "entities/goblinquest.h"
 #include "items/questitem.h"
 #include "game.h"
+
+using std::find_if;
+using std::make_shared;
 
 namespace lab3 {
 
   OldMan::OldMan(const WorldCoord& initial_position) : OldMan(initial_position, "Old man") {}
 
-  OldMan::OldMan(const WorldCoord& initial_position, const string& name) : Human(initial_position, name) {}
+  OldMan::OldMan(const WorldCoord& initial_position, const string& name) : Human(initial_position, name),
+    ai_state_function(AI_STATE_WAIT_FOR_SHINY_THING) {}
 
   bool OldMan::is_shiny_thing(const CarriedItem& item) const {
     return is_type<CarriedShinyThing, CarriedItem>(item);
@@ -44,5 +53,46 @@ namespace lab3 {
       say(game, {"This is not my favourite shiny thing."});
     }
   }
+
+  void OldMan::do_tick(Game& game) {
+    if(ai_state_function) {
+      ai_state_function(*this, game);
+    }
+  }
+
+  function<void(OldMan&, Game&)> OldMan::AI_STATE_WAIT_FOR_SHINY_THING = [](OldMan& self, Game& game) {
+    if(self.has_shiny_thing()) {
+      game.get_world().add_entity(make_shared<GoblinQuest>(game, self.get_position().map_id));
+
+      self.say(game, {"AIIIIIEEE! GOBLINS!"});
+
+      self.ai_state_function = AI_STATE_GOBLINS_PANIC;
+    }
+  };
+
+  function<void(OldMan&, Game&)> OldMan::AI_STATE_GOBLINS_PANIC = [](OldMan& self, Game& game) {
+    const auto adjacents = self.get_adjacent_entities(game, DIR_SOUTH);
+    auto door_it = find_if(adjacents.begin(), adjacents.end(), points_to_type<Door, shared_ptr<Entity>>);
+
+    if(door_it == adjacents.end()) {
+      if(!adjacents.empty()) {
+        self.say(game, "shouts", { "Get out of my way!" });
+      }
+      self.move(game, DIR_SOUTH);
+    } else {
+      if(!(*door_it)->is_solid()) {
+        (*door_it)->activated_by(game, self);
+      }
+      self.say(game, { "Whew." });
+      self.ai_state_function = AI_STATE_GO_TO_NORTH_WALL;
+    }
+  };
+
+  function<void(OldMan&, Game&)> OldMan::AI_STATE_GO_TO_NORTH_WALL = [](OldMan& self, Game& game) {
+    self.move(game, DIR_NORTH);
+    if(! self.get_adjacent_tile(game, DIR_NORTH) -> can_enter(self)) {
+      self.ai_state_function = nullptr;
+    }
+  };
 
 }
--- 3lab/examplegame/actors/oldman.h
+++ 3lab/examplegame/actors/oldman.h
@@ -1,10 +1,12 @@
 #pragma once
+#include <functional>
 #include <string>
 #include <vector>
 
 #include "actors/human.h"
 #include "inputer.h"
 
+using std::function;
 using std::string;
 using std::vector;
 
@@ -12,8 +14,16 @@ namespace lab3 {
 
   class OldMan : public Human {
 
+    function<void(OldMan&, Game&)> ai_state_function;
+
+    static function<void(OldMan&, Game&)> AI_STATE_WAIT_FOR_SHINY_THING;
+    static function<void(OldMan&, Game&)> AI_STATE_GOBLINS_PANIC;
+    static function<void(OldMan&, Game&)> AI_STATE_GO_TO_NORTH_WALL;
+
     bool has_shiny_thing() const;
     bool is_shiny_thing(const CarriedItem& item) const;
+
+    virtual void do_tick(Game& game) override;
 
     public:
       OldMan(const WorldCoord& initial_position);
