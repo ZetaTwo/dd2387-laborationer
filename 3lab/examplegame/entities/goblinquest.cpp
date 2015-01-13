e47216609e73eccf12c97961bbaa5bd20ed025a7
Emil Lundberg <lundberg.emil@gmail.com>
2015-01-13 21:01:23 +0100

    Make goblins fit on the map

--- 3lab/examplegame/entities/goblinquest.cpp
+++ 3lab/examplegame/entities/goblinquest.cpp
@@ -4,7 +4,7 @@ namespace lab3 {
 
   GoblinQuest::GoblinQuest(Game& game, Identifiable::identifier_t map_id) : finished(false) {
     for (Coord::coord_t i = 0; i < 3; i++) {
-      shared_ptr<PhysicalEntity> goblin = make_shared<Goblin>(WorldCoord{ map_id, 15, 12 + 2 * i });
+      shared_ptr<PhysicalEntity> goblin = make_shared<Goblin>(WorldCoord{ map_id, 15, 10 + 2 * i });
       game.get_world().add_physical(goblin);
       goblins.push_back(goblin);
     }
