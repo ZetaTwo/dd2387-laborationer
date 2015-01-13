6de0c9fedf6b3d0a1d1f2c518b56734f08f3341c
Emil Lundberg <lundberg.emil@gmail.com>
2015-01-13 20:53:08 +0100

    Make doors indicate their state

--- 3lab/examplegame/entities/door.h
+++ 3lab/examplegame/entities/door.h
@@ -17,7 +17,7 @@ namespace lab3 {
     virtual bool is_solid()   const override { return !open; }
     virtual bool is_visible() const override { return true; }
     virtual char to_char()    const override {
-      return 'X';
+      return open ? 'L' : 'D';
     }
 
   };
